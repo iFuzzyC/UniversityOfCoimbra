38e95d7d41539f7c192f7d618f2035c268d954e5
iFuzzyC <34630228+iFuzzyC@users.noreply.github.com>
2017-12-30 16:30:37 -0200

    Update ex1-2.c

--- MPI/TrabalhoTres/Ex1/ex1-2.c
+++ MPI/TrabalhoTres/Ex1/ex1-2.c
@@ -1,57 +1,5 @@
 #include <mpi.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <unistd.h>
-
-#define PI 3.1415927
-
-double*alocaVetor(int n){
-	double *v;
-	v = malloc(n*sizeof(double));
-	return v;
-}
-
-double**alocaMatriz(int rows, int col){
-	double**M;
-	int i;
-	M = malloc(rows*sizeof(double*));
-	for(i = 0; i < rows; i++) M[i] = malloc(col*sizeof(double));
-	return M;
-}
-
-void iniciaVetor(int n, int indice, double*v, double**M){
-	int i;
-	for(i = 0; i < n; i++){
-		v[i] = M[i][indice];
-	}
-}
-
-void iniciaMatriz(int rows, int col, double**M, int rank){
-	int i, j;
-	for(i = 0; i < rows; i++){
-		for(j = 0; j < col; j++){
-			M[i][j] = j + 1 + (i + rows*rank)*PI;
-		}
-	}
-}
-
-double VetorxVetor(int n, double*a, double*b){
-	int i;
-	double sum = 0;
-	for(i = 0; i < n; i++){
-		sum += a[i]*b[i];
-	}
-	return sum;
-}
-
-void printM(double**A, const int rows, const int col){
-	int i, j;
-	for(i = 0;i < rows; i++){
-		for(j = 0; j < col; j++) printf("%lf ", A[i][j]);
-		printf("\n");
-	}
-}
+#include "operations.h"
 
 int main(int argc, char*argv[]){
 	
